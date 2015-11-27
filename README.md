6d21ba0041dbb66aa76975b9803b47bf12ed421c
yonpyon <mkeilani43@gmail.com>
2015-11-26 17:28:13 -0800

    final?

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ codes and sources
 ---------------
 N/A
 
+what did the person say to the turkey?
+
+nothing. cause the turkey is dead because of the consumerism holiday
+called thanksgiving. thankgiving, not having the turkey in the first place, is a holiday 
+that celebrates the falsehood of america's ,making and the killings of native peoples and 
+we could go on but this joke isnt funny anymore,..... 
--- game.js
+++ game.js
@@ -37,5 +37,54 @@ while( p3!= "APPLES"){
 
 	}
 }
+
 alert("your right " + name + " !" );
+
+
+var p4 = prompt("what can i draw most of the time?").toUpperCase();
+while (p4!="HUMANS"){
+	if (p4 === "ANIMALS") {
+		alert("i wish... m(_ _; )m");
+		p4 = prompt("what can i draw most of the time?").toUpperCase();
+	};
+	if else(p4 === "LANDSCAPES"){
+		alert("i cant bob ross.... actually i can, but i get bored.");
+		p4 = prompt("what can i draw most of the time?").toUpperCase();
+	}
+	if else(p4 === "ANIME"){
+		alert("if i had a penny for everytime i got this question id kill every person with pennys that asked my that question. no.");
+		p4 = prompt("what can i draw most of the time?").toUpperCase();
+	}
+	else{
+		alert("nope!")
+		p4 = prompt("what can i draw most of the time?").toUpperCase();
+	}
+}
+alert("yep!! im a cartoonist");
+
+var p5= prompt("am i listening to youtube videos right now? (true or false)");{
+	if (false) {
+		alert("no")
+		p5= prompt("am i listening to youtube videos right now? (true or false)");
+	}
+	else{
+		alert("true or false man.... just true or false.");
+	}
+}
+alert("yep.... deathgrips man....");
+
+var p6 = prompt("how old am i?");{
+	if (p6=== 16){
+		alert("yep");
+	}
+	else{
+		if(p6 < 16){
+			alert("nuh uh! go higer");
+		}
+		else if(p6> 16){
+			alert("NOPE! :DDD go lower");
+		}
+	}
+}
+
 alert("thats all... yep thats it");
